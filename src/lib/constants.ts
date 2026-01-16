bdee58684b46402a5cc3ddf6f630250331532448
ATEEQ KHAN <ateeqhussainkhan02@gmail.com>
2026-01-16 12:59:42 +0530

    Fix merge conflict markers in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Phone number constants
 export const PHONE_NUMBER = "919392554899";
 export const PHONE_DISPLAY = "+91 93925 54899";
@@ -7,13 +6,3 @@ export const PHONE_TEL = `+${PHONE_NUMBER}`;
 // Email constant
 export const EMAIL = "book@ameenatravels.online";
 export const EMAIL_HREF = `mailto:${EMAIL}`;
-=======
-// Phone number constants
-export const PHONE_NUMBER = "919392554899";
-export const PHONE_DISPLAY = "+91 93925 54899";
-export const PHONE_TEL = `+${PHONE_NUMBER}`;
-
-// Email constant
-export const EMAIL = "book@ameenatravels.online";
-export const EMAIL_HREF = `mailto:${EMAIL}`;
->>>>>>> c960db0 (Updated code)
